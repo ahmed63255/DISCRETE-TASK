590f284a58f59723b8b637bb45150ce3e80fe3ea
ahmed63255 <121638723+ahmed63255@users.noreply.github.com>
2022-12-29 20:29:51 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("                       this program print prime numbers from first num to second num ."); 
+            Console.WriteLine("enter first num : ");
+            int firstnum=int.Parse(Console.ReadLine()); 
+            Console.WriteLine("enter second num : ");
+            int secondnum = int.Parse(Console.ReadLine());
+            Console.WriteLine("  ");
+            for(int i = firstnum; i <= secondnum; i++)
+            {
+                int counter = 0;
+                for(int j=2;j<=i/2;j++)
+                {
+
+                    if(i%j==0)
+                    {
+
+                        counter++;
+                        break;
+
+
+                    }
+                }
+                if (counter == 0 && i != 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+
+
+            }
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+
+    }
+    }
+
